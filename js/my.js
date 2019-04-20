eb39eb8dcbb5b80c67445ebafd8c844e368f7cdc
Alexandra Smirnova <alexsashka2208@gmail.com>
2019-04-20 23:46:20 +0300

    adding my files to my repository

---
+++ js/my.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const classList = document.body.classList;
+const h = document.getElementsByTagName('h1')[0];
+
+if (document.documentElement.clientWidth > 768) {
+  h.classList.add('mt-5');
+}
